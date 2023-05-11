7fc784e2eac4cba54aaad59a8aff4acefda0864a
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-11 14:26:08 +0000

    create input values

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import './App.css'
 
 export default function App() {
@@ -9,10 +9,67 @@ export default function App() {
   const [downPayment, setDownPayment] = useState(0);
   const [tenure, setTenure] = useState(12);
   const [emi, setEmi] = useState(0);
+
+  const updateEMI = () => {console.log("EMI")}
+  const updateDownPayment = () => {console.log("downPayment")}
   
   return (
-    <div classNam='App'>
-      <span className='title'>EMI Calculator</span>
+    <div className='App'>
+      <span className="title" style={{ fontSize: 30, marginTop: 10 }}>
+        EMI Calculator
+      </span>
+
+       <span className="title">
+        Total cost of Asset
+      </span>
+      <input
+        type="number"
+        value={cost}
+        onChange={(e) => setCost(e.target.value)}
+        placeholder="Total cost of Asset"
+      />
+
+      <span className="title">
+        Interest Rate (in %)
+      </span>
+      <input
+        type="number"
+        value={interest}
+        onChange={(e) => setInterest(e.target.value)}
+      />
+
+      <span className="title">
+        Processing Fee (in %)
+      </span>
+      <input
+        type="number"
+        value={fee}
+        onChange={(e) => setFee(e.target.value)}
+      />
+      
+        <span className="title">
+        Down Payment
+      </span>
+      <input
+        type="range"
+        min={0}
+        max={cost}
+        value={downPayment}
+        onChange={updateEMI}
+        className="slider"
+      />
+
+      <span className="title">
+        Loan per Month
+      </span>
+      <input
+        type="range"
+        min={0}
+        max={cost}
+        value={emi}
+        onChange={updateDownPayment}
+        className="slider"
+      />
     </div>
   )
 }
