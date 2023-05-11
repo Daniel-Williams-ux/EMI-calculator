aec414af350036b862206a756c6663bb50cbe2cb
Daniel-Williams-ux <williamsoligodaniel@gmail.com>
2023-05-11 06:25:18 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css'
+
+export default function App() {
+
+  const [cost, setCost] = useState(0);
+  const [interest, setInterest] = useState(10);
+  const [fee, setFee] = useState(1);
+  const [downPayment, setDownPayment] = useState(0);
+  const [tenure, setTenure] = useState(12);
+  const [emi, setEmi] = useState(0);
+  
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+    </main>
+  )
+}
